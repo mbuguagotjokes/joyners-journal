16be35f577846ddea39e89cddab340f0b3ee5496
mbuguagotjokes <calvinmbuguam@gmail.com>
2026-05-14 16:37:21 +0300

    Moved from gh-pages to vercel ig🙄

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,11 +10,6 @@ const config = {
 		adapter: adapter({
 			fallback: "index.html"
 		}),
-		paths: {
-  		base: process.env.NODE_ENV === 'production'
-  			? '/joyners-journal'
-  			: ''
-		}
 	}
 };
 
