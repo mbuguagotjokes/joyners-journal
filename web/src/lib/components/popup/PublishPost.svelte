b2b1f32af0b7344b053ca34972cb8ba1eaa2d112
mbuguagotjokes <calvinmbuguam@gmail.com>
2026-05-16 16:37:32 +0300

    Added link stnr.mbuguaaaaaa.xyz

--- web/src/lib/components/popup/PublishPost.svelte
+++ web/src/lib/components/popup/PublishPost.svelte
@@ -10,69 +10,97 @@
 
     let qrurl = $state("")
 
+    let link = $state("")
+
     onMount(async() => {
-        const qrCode = new QRCodeStyling({
-            width: 200,
-            height: 200,
-            data: page.url.origin + "/p/" + slug,
-            dotsOptions: {
-                color: "#02010A",
-                type: "rounded",
-            },
-            backgroundOptions: {
-                color: "#fff",
-            },
-        });
+        let rl = page.url.origin + "/p/" + slug
+        try {
+            let sl = await fetch("https://stnr.mbuguaaaaaa.xyz/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ url: rl })
+            })
 
-        // Get Base64
-        qrCode.getRawData("svg").then((blob) => {
-            const reader = new FileReader();
+            let response = await sl.json();
+            if (sl.ok) {
+                link = response.data
+                console.log(link)
+                const qrCode = new QRCodeStyling({
+                    width: 200,
+                    height: 200,
+                    data: link,
+                    dotsOptions: {
+                        color: "#02010A",
+                        type: "rounded",
+                    },
+                    backgroundOptions: {
+                        color: "#fff",
+                    },
+                });
+        
+                // Get Base64
+                qrCode.getRawData("svg").then((blob) => {
+                    const reader = new FileReader();
+        
+                    reader.onloadend = () => {
+                        const base64 = reader.result;
+                        qrurl = base64?.toString() as string
+                    };
+        
+                    reader.readAsDataURL(blob);
+                });
+            } else {
+                sendLocalNotification("warning", "Failed to shorten link, using link as is", 6000)
+                link = rl
+            }
+        } catch (error) {
+            sendLocalNotification("warning", "Unable to shorten link, using link as is", 6000)
+            link = rl
+        }
 
-            reader.onloadend = () => {
-                const base64 = reader.result;
-                qrurl = base64?.toString() as string
-            };
-
-            reader.readAsDataURL(blob);
-        });
     })
 </script>
 
-<section class="publish">
-    <img src={qrurl} alt="">
-    
-    <span>{qrurl === "" ? "" : "Scan the QRCode or"} <a href="/p/{slug}">click this link!</a></span>
-    <button
-        type="button"
-        onclick={async() => {
-            try {
-                await navigator.clipboard.writeText(page.url.origin + "/p/" + slug)
-                sendLocalNotification("success", "Copied to clipboard succesfully")
-            } catch (error) {
-                // Fallback for mobile/http
-                const textarea = document.createElement("textarea");
-                textarea.value = page.url.origin + "/p/" + slug;
-                
-                textarea.style.position = "fixed";
-                textarea.style.left = "-9999px";
-                
-                document.body.appendChild(textarea);
-                
-                textarea.focus();
-                textarea.select();
-                
-                const successful = document.execCommand("copy");
-                
-                document.body.removeChild(textarea);
-                
-                if (!successful) {
-                    sendLocalNotification("error", "Failed to copy to clipboard")
+{#if link}
+    <section class="publish">
+        <img src={qrurl} alt="">
+        
+        <span>{qrurl === "" ? "" : "Scan the QRCode or"} <a href="{link}">click this link!</a></span>
+        <button
+            type="button"
+            onclick={async() => {
+                try {
+                    await navigator.clipboard.writeText(link)
+                    sendLocalNotification("success", "Copied to clipboard succesfully")
+                } catch (error) {
+                    // Fallback for mobile/http
+                    const textarea = document.createElement("textarea");
+                    textarea.value = link
+                    
+                    textarea.style.position = "fixed";
+                    textarea.style.left = "-9999px";
+                    
+                    document.body.appendChild(textarea);
+                    
+                    textarea.focus();
+                    textarea.select();
+                    
+                    const successful = document.execCommand("copy");
+                    
+                    document.body.removeChild(textarea);
+                    
+                    if (!successful) {
+                        sendLocalNotification("error", "Failed to copy to clipboard")
+                    }
+                    sendLocalNotification("success", "Copied to clipboard succesfully")
                 }
-                sendLocalNotification("success", "Copied to clipboard succesfully")
-            }
-        }}
-    >Copy link</button>
-</section>
+            }}
+        >Copy link</button>
+    </section>
+{/if}
+
 
 <style>
     .publish {
